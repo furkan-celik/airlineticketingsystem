76ea9e55853a2283f996ff56102102c5a69e53c9
cinucen <cinucen@sabanciuniv.edu>
2020-04-22 19:12:41 +0300

    Address details added, some frontend implementations on profile settings part

---
+++ Areas/Identity/Pages/Account/Manage/AddressDetails.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Areas.Identity.Pages.Account.Manage
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly WebApplication1.Data.ApplicationDbContext _context;
+
+        public DetailsModel(WebApplication1.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Address Address { get; set; }
+
+        
+        public async Task<IActionResult> OnGetAsync(int? id, bool? saveChangesError = false)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Address = await _context.Addresses
+                .AsNoTracking()
+                .Include(a => a.Owner).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Address == null)
+            {
+                return NotFound();
+            }
+            
+            return Page();
+        }
+ 
+    }
+}
