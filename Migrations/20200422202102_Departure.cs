7efc60e1477be6548170e53b51af9825fe3d0794
furkan-celik <game.wive@gmail.com>
2020-04-22 23:22:17 +0300

    Destination -> Departure

---
+++ Migrations/20200422202102_Departure.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class Departure : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Destination",
+                table: "Flights");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Departure",
+                table: "Flights",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Departure",
+                table: "Flights");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Destination",
+                table: "Flights",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
