facdbdc31c38dcc05ebc91cbc3d34ef21f69079d
furkan-celik <game.wive@gmail.com>
2020-04-29 14:29:40 +0300

    Airport controlls added

---
+++ Controllers/AirportsController.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Authorize("ReqAdmin")]
+    public class AirportsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AirportsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Airports
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Airports.Include(a => a.City);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Airports/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var airport = await _context.Airports
+                .Include(a => a.City)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (airport == null)
+            {
+                return NotFound();
+            }
+
+            return View(airport);
+        }
+
+        // GET: Airports/Create
+        public IActionResult Create()
+        {
+            ViewData["CityId"] = new SelectList(_context.Cities, "CityId", "CityName");
+            return View();
+        }
+
+        // POST: Airports/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,AirportName,CityId")] Airport airport)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(airport);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CityId"] = new SelectList(_context.Cities, "CityId", "CityName", airport.CityId);
+            return View(airport);
+        }
+
+        // GET: Airports/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var airport = await _context.Airports.FindAsync(id);
+            if (airport == null)
+            {
+                return NotFound();
+            }
+            ViewData["CityId"] = new SelectList(_context.Cities, "CityId", "CityName", airport.CityId);
+            return View(airport);
+        }
+
+        // POST: Airports/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,AirportName,CityId")] Airport airport)
+        {
+            if (id != airport.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(airport);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AirportExists(airport.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CityId"] = new SelectList(_context.Cities, "CityId", "CityName", airport.CityId);
+            return View(airport);
+        }
+
+        // GET: Airports/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var airport = await _context.Airports
+                .Include(a => a.City)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (airport == null)
+            {
+                return NotFound();
+            }
+
+            return View(airport);
+        }
+
+        // POST: Airports/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var airport = await _context.Airports.FindAsync(id);
+            _context.Airports.Remove(airport);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AirportExists(int id)
+        {
+            return _context.Airports.Any(e => e.Id == id);
+        }
+    }
+}
