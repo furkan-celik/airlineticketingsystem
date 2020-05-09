66cbcf57514cbabbc5ad4ac76096d2e5cdead2d3
furkan-celik <game.wive@gmail.com>
2020-05-09 18:04:55 +0300

    Buy system adapted to reservation as well

--- Models/Reservation.cs
+++ Models/Reservation.cs
@@ -15,6 +15,8 @@ namespace WebApplication1.Models
         [Required]
         [ForeignKey("Flight")]
         public int FlightId { get; set; }
+        [Required]
+        public bool isChild { get; set; }
 
         public virtual AppUser Owner { get; set; }
         public virtual Flight Flight { get; set; }
