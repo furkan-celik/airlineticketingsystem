45544fecc3f58d5920b28062541311082e7e8b33
furkan-celik <game.wive@gmail.com>
2020-06-02 16:16:46 +0300

    prev/next buttons fixed

---
+++ Migrations/20200602122129_ResTime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class ResTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "processTime",
+                table: "Reservations",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "processTime",
+                table: "Reservations");
+        }
+    }
+}
